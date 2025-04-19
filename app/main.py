27ab31c20df0e18c0d7c85e7246ab6edc05d90ea
JasonCacante <jason.cacante@gmail.com>
2025-04-18 21:40:46 -0500

    feature: first endpoint read books

--- app/main.py
+++ app/main.py
@@ -3,6 +3,6 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-async def read_root():
+@app.get("/books")
+async def read_all_books():
     return {"message": "Hello from FastAPI with Docker, Postgres, and Redis!"}

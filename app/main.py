45587e0c045a1e77c9fa1a0f2bebfc68f73b4c48
JasonCacante <jason.cacante@gmail.com>
2025-05-29 19:13:00 -0500

    feature: add BookRequest model for book validation and update create_book endpoint

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import Body, FastAPI
+from pydantic import BaseModel, Field
+from typing import Optional
 
 app = FastAPI()
 
@@ -16,6 +18,18 @@ class Book:
         self.author = author
         self.description = description
         self.rating = rating
+
+
+class BookRequest(BaseModel):
+    book_id: Optional[int] = None
+    title: str = Field(min_length=3, max_length=100)
+    author: str = Field(min_length=3, max_length=100)
+    description: str = Field(
+        min_length=10, max_length=500, description="A brief description of the book"
+    )
+    rating: int = Field(ge=1, le=5, description="Rating of the book from 1 to 5")
+
+    # I need to add some kind of validation
 
 
 BOOKS = [
@@ -72,6 +86,14 @@ async def read_all_books():
 
 
 @app.post("/create_book")
-async def create_book(book=Body()):
-    BOOKS.append(book)
-    return {"message": "Book created successfully", "book": book}
+async def create_book(book: BookRequest):
+    new_book = Book(**book.model_dump())
+    new_book = find_book_id(new_book)
+    BOOKS.append(new_book)
+    return {"message": "Book created successfully", "book": new_book}
+
+
+def find_book_id(book: Book):
+    book.book_id = 1 if len(BOOKS) == 0 else BOOKS[-1].book_id + 1
+
+    return book
